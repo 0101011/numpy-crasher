bcce474d65cca58d0f1b7b70361dd80409f1e3af
Andrew Stepin <andrew.stepin@gmail.com>
2018-12-18 11:14:24 +0300

    Create 1_sine.py

---
+++ basic_operations/1_sine.py
@@ -0,0 +1,10 @@
+import matplotlib.pylab as plt
+import numpy as np 
+
+x = np.linspace(-np.pi, np.pi, 201)
+plt.plot(x, np.sin(x))
+plt.xlabel('Angle [rad]')
+plt.ylabel('sin(x)')
+plt.axis('tight')
+plt.grid()
+plt.show()
